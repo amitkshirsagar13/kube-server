1ef8172a5b0dd456243cf78097ec5500a68ab02f
Amit Kshirsagar <amitkshirsagar13@users.noreply.github.com>
2018-04-15 13:42:19 +0530

    Update deploy.sh

--- minikube/deploy.sh
+++ minikube/deploy.sh
@@ -3,6 +3,6 @@ cd
 wget https://github.com/amitkshirsagar13/kube-server/raw/master/minikube/bin.7z
 7z x bin.7z && rm bin.7z
 chmod 755 -R ./bin/*
-wget https://storage.googleapis.com/kubernetes-helm/helm-v2.8.2-linux-amd64.tar.gz && tar -zxvf helm-v2.8.2-linux-amd64.tar.gz && cp li*/helm . && rm -R lin* helm-v2.8.2-linux-amd64.tar.gz
+wget https://storage.googleapis.com/kubernetes-helm/helm-v2.8.2-linux-amd64.tar.gz && tar -zxvf helm-v2.8.2-linux-amd64.tar.gz && cp li*/helm . && rm -R lin* helm-v2.8.2-linux-amd64.tar.gz && ls -ltr
 mv helm ./bin/helm
 PATH=$HOME/bin:$PATH
